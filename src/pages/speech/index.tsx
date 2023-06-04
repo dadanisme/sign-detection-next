e50d3d1aa7d4bba4bf7e56dcf41cc7010ea16a28
Ramdan <dadanisme@upi.edu>
2023-06-05 00:18:46 +0700

    click for android

--- src/pages/speech/index.tsx
+++ src/pages/speech/index.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { BiMicrophone } from "react-icons/bi";
-import { BsChevronLeft } from "react-icons/bs";
+import { BsChevronLeft, BsSquareFill } from "react-icons/bs";
 import Link from "next/link";
 import Head from "next/head";
+import clsx from "clsx";
+
+const ANDROID = "Linux armv8l";
 
 export default function Speech() {
   const [recognizer, setRecognizer] = useState<SpeechRecognition>();
   const [result, setResult] = useState<string>();
+  const [os, setOs] = useState<string>();
+  const [isListening, setIsListening] = useState(false);
 
   const start = () => {
     setResult(undefined);
+    os === ANDROID && setIsListening(true);
     recognizer?.start();
   };
 
   const stop = () => {
+    os === ANDROID && setIsListening(false);
     recognizer?.stop();
   };
 
@@ -38,6 +45,7 @@ export default function Speech() {
     }
 
     navigator.mediaDevices.getUserMedia({ audio: true });
+    setOs(navigator.platform);
   }, []);
 
   return (
@@ -46,17 +54,30 @@ export default function Speech() {
         <title>Speech Recognition App</title>
       </Head>
       <button
-        className="border-2 border-white rounded-full p-20 text-white
-      hover:bg-white/75 hover:text-[#40128B] transition-all duration-300
-      hover:border-[#40128B] active:scale-95 active:bg-white/50"
-        onMouseDown={start}
-        onMouseUp={stop}
-        onTouchStart={start}
-        onTouchCancel={stop}
-        onTouchCancelCapture={stop}
-        onTouchEnd={stop}
+        className={clsx(
+          "border-2 rounded-full p-20 text-white",
+          "hover:bg-white/75 hover:text-[#40128B] transition-all duration-300",
+          "active:scale-95 active:bg-white/50",
+          isListening
+            ? "border-white animate-pulse bg-red-600"
+            : "hover:border-[#40128B] border-white"
+        )}
+        // different for android
+        onMouseDown={() => os !== ANDROID && start()}
+        onMouseUp={() => os !== ANDROID && stop()}
+        onTouchStart={() => os !== ANDROID && start()}
+        onTouchCancel={() => os !== ANDROID && stop()}
+        onTouchCancelCapture={() => os !== ANDROID && stop()}
+        onTouchEnd={() => os !== ANDROID && stop()}
+        onTouchEndCapture={() => os !== ANDROID && stop()}
+        // different for android
+        onClick={() => os === ANDROID && (isListening ? stop() : start())}
       >
-        <BiMicrophone className="font-bold text-7xl" />
+        {isListening ? (
+          <BsSquareFill className="font-bold text-6xl" />
+        ) : (
+          <BiMicrophone className="font-bold text-7xl" />
+        )}
       </button>
 
       {result && (
