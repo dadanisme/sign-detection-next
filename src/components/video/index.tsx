308f6f0869075d1cfc4a981ae5cd0f74a17c59a4
Ramdan <dadanisme@upi.edu>
2023-06-04 17:45:10 +0700

    update model

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -29,9 +29,8 @@ export default function Video({ responses, setResponses }: VideoProps) {
 
     const options: GestureRecognizerOptions = {
       baseOptions: {
-        modelAssetPath:
-          "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
-        delegate: "GPU",
+        modelAssetPath: "model.task",
+        delegate: "CPU",
       },
       runningMode: "VIDEO",
       numHands: 2,
